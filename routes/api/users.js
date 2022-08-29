ca32c3fb452e7efd701d32b87122346ac307165b
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-08-29 05:40:11 -0700

    fixed login error

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,8 +5,16 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const keys = require('../../config/keys');
 const jwt = require('jsonwebtoken');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.post('/register', (req,res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     // Check to make sure nobody has already registered with a duplicate email
     User.findOne({email: req.body.email})
         .then(user => {
@@ -20,17 +28,6 @@ router.post('/register', (req,res) => {
                     email: req.body.email,
                     password: req.body.password
                 })
-                jwt.sign(
-                    payload,
-                    keys.secretOrKey,
-                    { expiresIn: 3600 },
-                    (err, token) => {
-                        res.json({
-                            success: true,
-                            token: "Bearer " + token
-                        });
-                    }
-                )
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err,hash) => {
                         if (err) throw err;
@@ -65,7 +62,18 @@ router.post('/login', (req,res) => {
                             id: user.id,
                             handle: user.handle,
                             email: user.email
-                        }  
+                        }
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            }
+                        )
                     } else {
                         return res.status(400).json({ password: "Incorrect password"})
                     }
